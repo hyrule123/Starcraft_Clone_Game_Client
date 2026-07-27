23337f656b67ceaeb4d941f29a28db391dfa6fe9
HW <ekdrn1@naver.com>
2026-07-27 22:42:58 +0900

    UnitInputHandler 하드코딩 부분 제거

--- Proj/Content/Script/UnitInputHandler.cpp
+++ Proj/Content/Script/UnitInputHandler.cpp
@@ -51,15 +51,14 @@ namespace engine
 			ASSERT(cam);
 
 			//게임 해상도와 현재 윈도우 해상도가 다를 수 있으므로 비율을 구해서 곱해주어야 함
-			constexpr float width_ratio = (float)SC::kResolutionWidth / (float)kDefaultWidth;
-			constexpr float height_ratio = (float)SC::kResolutionHeight / (float)kDefaultHeight;
+			float width_ratio = (float)SC::kResolutionWidth / (float)EngineMain::GetInst().GetScreenWidth();
+			float height_ratio = (float)SC::kResolutionHeight / (float)EngineMain::GetInst().GetScreenHeight();
 
-			static_assert(kDefaultWidth == 1280U && kDefaultHeight == 960U, "Engine Default Resolution has changed, please update the code accordingly.");
-
+			//비율만큼 축소
 			mouse_pos.x *= width_ratio;
 			mouse_pos.y *= height_ratio;
 
-			//1. 화면의 가운데가 원점 -> 일치시키기
+			//화면의 가운데가 원점 -> 일치시키기
 			//윈도우 기준 좌상단이 0, 0 / 게임 기준 좌상단 -width / 2, height / 2
 			//윈도우 기준 우하단이 width, height / 게임 기준 width / 2, height / 2
 			// 선형 변환 시 GameX = (WindowX - width / 2), GameY = (height / 2 - WindowY)
